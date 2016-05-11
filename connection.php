52ab7cec6895ba08919b51953103c1e47105174d
Ana Oprescu <ana.oprescu28@gmail.com>
2016-05-11 20:16:43 +0300

    changed

--- connection.php
+++ connection.php
@@ -1,13 +1,14 @@
 <?php
-   $host = "ec2-54-228-219-2.eu-west-1.compute.amazonaws.com";
-   $port = "5432";
-   $dbname = "d71e3qs21dn1mv";
-   $dbpass="x8tnmXrUAjZdKAGlF200aOmjw8";
+try {
 
-   $db = pg_connect( "$host $port $dbname $bdpass"  );
-   if(!$db){
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
+  $dbh = new PDO("postgres://telenaljjwdnsd:x8tnmXrUAjZdKAGlF200aOmjw8@ec2-54-228-219-2.eu-west-1.compute.amazonaws.com:5432/d71e3qs21dn1mv");
+  //echo "PDO connection object created";
+  $dbh->prepare("SELECT * FROM city");
+  $dbh->execute();
+  $test=$dbh->fetch(PDO::FETCH_ASSOC);
+  print_r($test);
+}
+catch(PDOException $e) {
+  echo 'Connection failed: ' . $e->getMessage();
+}
 ?>
